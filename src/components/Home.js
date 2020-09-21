6ec9b48fd583262c40d2b8e6f5a29149edc3f4e7
Robin Wen <blockxyz@gmail.com>
2020-09-21 15:59:29 +0800

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Interactive from 'react-interactive';
+import { Link } from 'react-router-dom';
+import { Code } from '../styles/style';
+import s from '../styles/home.style';
+
+export default function Home() {
+  const repoReadmeLink = text => (
+    <Interactive
+      as="a"
+      {...s.link}
+      href="https://github.com/dbarobin/robin.finance#readme"
+    >{text}</Interactive>
+  );
+
+  return (
+    <div>
+      <p style={s.p}>
+        I'm a partner of <Code>Exin</Code>, a startup firm focused on cryptocurrencies.
+      </p>
+
+      <div style={s.pageLinkContainer}>
+        <Interactive
+          as={a}
+          {...s.link}
+          href="https://twitter.com/vrwio"
+        >@vrwio</Interactive>
+      </div>
+    </div>
+  );
+}
